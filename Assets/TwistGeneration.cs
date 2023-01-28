33078976db4b3fe182c683942ba5dcb92081ea0e
alexman37 <62260530+alexman37@users.noreply.github.com>
2023-01-27 23:49:41 -0500

    Basic UI Layout
    Lays out the UI for the game. The chess board is in the middle, with each player's 3 status effects shown on the sides. The 3 global effects of the game are shown in the top right.
    
    NOTE: UI not scaled to screen size yet. Probably breaks on some resolutions

---
+++ Assets/TwistGeneration.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TwistGeneration : MonoBehaviour
+{
+    //realistically, there should be no more than 3 twists in a game, otherwise it's complete chaos
+    public int num_twists = 3;
+    public Image[] chosenTwistIcons;
+    public Sprite[] twistIcons;
+    public TextMeshProUGUI[] twistNames;
+
+    //currently there are:
+    // 29 twists
+    public enum Twist {REVOLUTION, CTF, KOTH, WIPEOUT, NO_MANS_LAND, TRENCHES, PESTILENCE, MINESWEEPER,
+        SNAKE, X2LONG, X2WIDE, HEARTS_AND_MINDS, BATTLE_ROYALE, WEATHER, FOG_OF_WAR, BOXING, MIRROR,
+        BLACK_HOLE, WATER, RIVER, ONE_TWO_PUNCH, ASH_AND_SNOW, NECRONOMICON, STALIN, MERCS,
+        ATTRITION, MIAMI, HORDE, ROULETTE};
+
+    //short names for each twist. temporary, will eventually be replaced.
+    private string[] temporaryNames = { "Rev", "CTF", "KOTH", "Wipeout", "NML", "Trench", "Plague", "Sweep",
+    "Snake", "Long", "Wide", "H&M", "B.R.", "Weather", "FogOfWar", "Boxing", "Mirror",
+    "B.H.", "10in", "River", "1-2", "A&S", "Necro", "Stalin", "Mercs", "Attr.", "Miami", "Undead", "Roulette"};
+
+    public Twist[] chosenTwists;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //choose the twists to be used
+        chosenTwists = new Twist[num_twists];
+        for(int i=0; i<num_twists; i++)
+        {
+            int sel = (int) Mathf.Floor(Random.Range(0.0f, 28.999f));
+            //get element of enum by index
+            Twist t = (Twist)sel;
+            chosenTwists[i] = t;
+
+            //assign pictures in the UI
+            chosenTwistIcons[i].sprite = twistIcons[sel];
+            twistNames[i].text = temporaryNames[sel];
+            //TODO: actually apply effects of the twists in code as well...
+            //   (probably each having their own script to run.)
+            
+        }
+
+        
+
+
+        
+    }
+}

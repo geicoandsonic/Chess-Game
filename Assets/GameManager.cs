466ba4ff7e1c3237d4920ca0b08f5f31bb1b62e6
alexman37 <62260530+alexman37@users.noreply.github.com>
2023-01-27 22:29:39 -0500

    Turn UI
    Some simple UI that tells you whose turn it is, and how many total have been played

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI turnKeeper; //used to keep track of turns
+    public TextMeshProUGUI turnCounter;
+    private int turnCountInt = 1;
+
     public bool playerHasPiece;
     public Unit playerPiece;
     public bool playerOneTurn; //True if white chess side is playing i.e. player one, false if black chess side is playing i.e. player two.
@@ -14,6 +19,8 @@ public class GameManager : MonoBehaviour
 
     void Awake(){
         gameState = GameState.PLAYERONETURN; //Should be start on scene start, but for now its just playeroneturn
+        turnKeeper.text = "WHITE to play"; //change this as well
+        turnKeeper.color = Color.white;
         playerHasPiece = false;
     }
 
@@ -25,23 +32,36 @@ public class GameManager : MonoBehaviour
     public void changeGameState(){
         if(gameState == GameState.START){
             gameState = GameState.PLAYERONETURN;
+            turnKeeper.text = "WHITE to play";
+            turnKeeper.color = Color.white;
         }
         else if(gameState == GameState.PLAYERONETURN){
             gameState = GameState.PLAYERTWOTURN;
+            turnKeeper.text = "BLACK to play";
+            turnKeeper.color = Color.gray;
         }
         else if(gameState == GameState.PLAYERTWOTURN){
             gameState = GameState.PLAYERONETURN;
+            turnKeeper.text = "WHITE to play";
+            turnKeeper.color = Color.white;
+            //a "turn" ends when black makes its move
+            turnCountInt++;
+            turnCounter.text = "Turn #" + turnCountInt;
         }
     }
 
     public void timeout(){
         tempState = gameState;
         gameState = GameState.TIMEOUT;
+        turnKeeper.text = "[!]  TIMEOUT  [!]";
+        turnKeeper.color = Color.red;
     }
 
     public void endTimeout(){
         gameState = tempState;
         tempState = GameState.DEFAULT;
+        turnKeeper.text = "[!]  DEFAULT  [!]";
+        turnKeeper.color = Color.blue;
     }
 
 }
